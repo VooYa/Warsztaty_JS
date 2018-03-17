e4b0141bb81dd52fb967c299576001b6925f4cdb
Karol <vooya84@gmail.com>
2018-03-17 15:26:01 +0100

    zmiany

---
+++ js/app — kopia.js
@@ -0,0 +1,15 @@
+console.log("działa!");
+var myName = 'Karol';
+var names = ['Ola', 'Michał', 'Kuba', 'Karol'];
+console.log(myName);
+console.log(names[1]);
+var a = 15;
+var b = 33;
+if (a > b) {
+    console.log("Hurra!")
+} else {
+    console.log("niestety pierwsza liczba jest mniejsza")
+};
+for (var i = 0; i <= names.length -1; i++) {
+    console.log(names[i])
+}
